80e71167c717a269ad3e7f7210ea62c2d41a0513
ekapangaribuan <ekapangaribuan@localhost>
2022-11-28 20:18:36 +0700

    commit delete feature

---
+++ Test Suites/POST/Delete Posts.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Delete Posts</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7e127be9-359d-4e25-9b09-9bfeb31c77bd</testSuiteGuid>
+   <testCaseLink>
+      <guid>11de0fef-8e0f-43aa-9866-e0b7b1f0a42e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POSTS/Delete Posts</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>7f0da3d1-41d6-4bde-8332-4bb5eb4d002c</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DeleteData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7f0da3d1-41d6-4bde-8332-4bb5eb4d002c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>id</value>
+         <variableId>65f2d117-a3cf-479d-a09d-db5929b36cfc</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
