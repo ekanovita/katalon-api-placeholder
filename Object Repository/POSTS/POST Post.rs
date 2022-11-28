4219d89df426a295aaf01b0a63c3a7ad31b693bc
ekapangaribuan <ekapangaribuan@ekas-mbp>
2022-11-28 12:52:50 +0700

    push first project

---
+++ Object Repository/POSTS/POST Post.rs
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>POST Post</name>
+   <tag></tag>
+   <elementGuidId>3bdb62ac-b01e-4e23-9c2c-9355f891e871</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n   \&quot;title\&quot;:\&quot;${title}\&quot;,\n   \&quot;body\&quot;:\&quot;${body}\&quot;,\n   \&quot;userId\&quot;:\&quot;${userId}\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <katalonVersion>8.0.1</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>${GlobalVariable.BaseURL}/posts</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>8037308e-2f48-4a29-b968-e53c67061559</id>
+      <masked>false</masked>
+      <name>title</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>d2577343-3341-48fb-b862-5c7e9715f9db</id>
+      <masked>false</masked>
+      <name>body</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>65351460-582f-45cb-b742-791602eee891</id>
+      <masked>false</masked>
+      <name>userId</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+
+
+WS.verifyResponseStatusCode(response, 201)
+
+assertThat(response.getStatusCode()).isEqualTo(201)
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/POST/Create Posts.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create Posts</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e8c2c42f-0dc7-4f5c-91dd-1c8187836e06</testSuiteGuid>
+   <testCaseLink>
+      <guid>0d24f690-c5cf-477e-bf43-6d333460a768</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POSTS/Create Posts</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>d245a5bd-0551-46d9-8ccf-04776aafdf7b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/JsonPlacerDATA</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>d245a5bd-0551-46d9-8ccf-04776aafdf7b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>title</value>
+         <variableId>00fbfc01-fa66-45d3-ac31-b62b4dce0113</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d245a5bd-0551-46d9-8ccf-04776aafdf7b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>body</value>
+         <variableId>d298d4cd-4f5f-40af-98df-f32c44cde0b8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d245a5bd-0551-46d9-8ccf-04776aafdf7b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>userId</value>
+         <variableId>545e6a1f-352d-4409-8f0a-02cb388fa4ec</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
