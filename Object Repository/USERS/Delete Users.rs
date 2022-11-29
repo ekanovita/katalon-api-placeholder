7764f7bc55c700dfaf6ca702f00776328b5f43d1
ekapangaribuan <ekapangaribuan@localhost>
2022-11-29 20:48:25 +0700

    commit feature Users

---
+++ Object Repository/USERS/Delete Users.rs
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Delete Users</name>
+   <tag></tag>
+   <elementGuidId>c0cda49f-b4a4-4e6b-b657-1b9440e839ad</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.0.1</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>DELETE</restRequestMethod>
+   <restUrl>${GlobalVariable.BaseURL}/users/${id}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'1'</defaultValue>
+      <description></description>
+      <id>aa1c013c-b213-4a94-b4c9-ecded434253f</id>
+      <masked>false</masked>
+      <name>id</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/USERS/Get Users.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get Users</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cea405d8-8ee6-4812-9e9e-7e4627a97458</testSuiteGuid>
+   <testCaseLink>
+      <guid>22947e73-4692-4279-9d8c-cd5c3f8ab68e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/USERS/Get Users</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>80211c79-34bf-4698-91ca-00dde281084b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DataUsers</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>80211c79-34bf-4698-91ca-00dde281084b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>id</value>
+         <variableId>a2187da4-8d5b-4ac0-81f8-55fe5cdcf4f6</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>80211c79-34bf-4698-91ca-00dde281084b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>name</value>
+         <variableId>59d9bfda-ddde-4bac-b36e-464a9e69a27a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>80211c79-34bf-4698-91ca-00dde281084b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>aeb573c7-07eb-41e0-9391-77bf1ac5c4a0</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>80211c79-34bf-4698-91ca-00dde281084b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>aa881600-e3b5-4210-a26f-777774808e50</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
